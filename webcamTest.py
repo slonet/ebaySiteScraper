187d38a615592938c4785337da81124c83181c49
Tyler Slone <tyler.slone47@gmail.com>
2018-11-19 20:30:17 -0800

    removing gui code from test script

--- webcamTest.py
+++ webcamTest.py
@@ -4,10 +4,6 @@ cam = VideoCapture(0)
 s, img = cam.read()
 
 if s:
-	namedWindow('Camera Test')
-	imshow('Camera Test', img)
-	waitKey(0)
-	destroyWindow('Camera Test')
-	#imwrite('testImage.jpg',img)
+	imwrite('testImage.jpg',img)
 else:
 	print('Could not capture an image')
